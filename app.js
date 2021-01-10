2079331402a33a468ca8b226044eb0b2ff9df9e3
winay05 <vinay.manipal05@gmail.com>
2021-01-10 14:16:42 +0530

    Adding cors middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -25,6 +26,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //1. GLOBAL MIDDLEWARES
+//implement cors
+app.use(cors());
+// Access-Control-Allow-Origin *
+app.options('*', cors());
+
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
