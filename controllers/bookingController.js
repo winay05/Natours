6d73aaf0024db388758955c9196527183a31b52c
winay05 <vinay.manipal05@gmail.com>
2021-01-10 15:10:15 +0530

    correcting typos in bookingController

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -38,32 +38,31 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session
   });
 });
-const createBookingCheckout = catchAsync(async session => {
+const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
-});
+};
 
 exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers('stripe-signature');
+  const signature = req.headers['stripe-signature'];
+
   let event;
   try {
-    event = stripe.webhooks.contructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
-    return res.status(400).send(`Webhook error:${err.message}`);
+    return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
-  res.status(200).json({
-    received: true
-  });
+  res.status(200).json({ received: true });
 };
 
 exports.getAllBookings = factory.getAll(Booking);
